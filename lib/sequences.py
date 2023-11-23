4b8633a7733383ae936fb518ce33c015d454dd50
calebmutisya <mutisyacalebmusau@gmail.com>
2023-11-23 15:20:00 +0300

    Completed

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib_seq=[0,1]
+    
+    while len(fib_seq) < length:
+        next_fib= fib_seq[-1] + fib_seq[-2]
+        fib_seq.append(next_fib)
+    print(fib_seq[:length])
